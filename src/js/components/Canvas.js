33a8707d73fcc668a8824439674e5156eeb87ee7
ted <ted@warpedpuppy.com>
2017-08-18 15:51:15 -0700

    solved resize

--- src/js/components/Canvas.js
+++ src/js/components/Canvas.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { WindowResizeListener } from 'react-window-resize-listener'
-
 
 export default class Canvas extends React.Component {
+
+
+
+
+
   render() {
-      console.log(this.props.size)
+
     return (
       <div>
-        <div id="background_canvas" ></div>
+
+              <div id="background_canvas" ></div>
+
+
         </div>
     );
   }
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -6,32 +6,29 @@ import Canvas from "./Canvas";
 import InputDiv from "./InputDiv";
 import { WindowResizeListener } from 'react-window-resize-listener'
 
-
-
 export default class Layout extends React.Component {
   constructor() {
     super();
     this.state = {
       title: "Welcome"
+
     };
   }
-
-
-
 
   render() {
 
 
     return (
-
       <div>
       <WindowResizeListener onResize={windowSize => {
-      console.log('Window height', windowSize.windowHeight)
-      console.log('Window width', windowSize.windowWidth)
-      this.state.windowSize = windowSize;
+
+      document.getElementById('background_canvas').setAttribute("style", "width:"+windowSize.windowWidth+"px;height:"+windowSize.windowHeight+"px;");
+
+
     }}/>
+
         <Header changeTitle={"asfd"} title={this.state.title} />
-        <Canvas size={this.state.windowSize} />
+        <Canvas />
         <InputDiv />
         <Footer />
       </div>
